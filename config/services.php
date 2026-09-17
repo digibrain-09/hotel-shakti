38a8dd49b6948058b3ad4c81b2a1f8c5f7bd4855
yashjoshipbr <yashjoshipbr@gmail.com>
2026-09-17 19:12:46 +0530

    Add project files

---
+++ config/services.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+    'encryption' => [
+        'type' => env("ENC_TYPE", "AES-128-ECB"),
+        'secret' => env("ENC_SECRET", "bLDbkv9y?wy@XDtp"),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+    'vapid' => [
+        'public'  => env('VAPID_PUBLIC'),
+        'private' => env('VAPID_PRIVATE'),
+        'subject' => env('VAPID_SUBJECT'),
+    ],
+
+
+];
